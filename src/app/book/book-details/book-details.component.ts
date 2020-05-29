d3929dacfc556937fbdd083a56427b83c4f291d2
Marek Matczak <mmatczak@autograf.pl>
2020-05-29 14:18:15 +0200

    reactive forms

--- src/app/book/book-details/book-details.component.ts
+++ src/app/book/book-details/book-details.component.ts
@@ -3,7 +3,7 @@ import {Book} from '../book';
 import {BookService} from '../book.service';
 import {ActivatedRoute, Data, Router} from '@angular/router';
 import {map, pluck, tap} from 'rxjs/operators';
-import {Observable} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-book-details',
@@ -12,36 +12,56 @@ import {Observable} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookDetailsComponent {
-  book$: Observable<Book>;
+  bookForm: FormGroup;
   private bookId: number | undefined;
 
   constructor(private readonly books: BookService,
               private readonly router: Router,
               route: ActivatedRoute) {
-    this.book$ = route.data
+    this.bookForm = new FormGroup({
+      author: new FormControl(null,
+        [Validators.required, Validators.maxLength(15)]),
+      title: new FormControl(null, Validators.required)
+    });
+
+    route.data
       .pipe(
         getBook(),
         createEmptyBookIfNoBookAvailable(),
         this.setBookId()
-      );
+      )
+      .subscribe(book => this.bookForm.patchValue(book));
   }
 
   private setBookId() {
     return tap<Book>(book => this.bookId = book.id);
   }
 
-  save(event: Event) {
-    event.preventDefault();
-    const formElement = event.target as HTMLFormElement;
-    const authorElement = formElement.querySelector<HTMLInputElement>('#author');
-    const author = authorElement.value;
-    const titleElement = formElement.querySelector<HTMLInputElement>('#title');
-    const title = titleElement.value;
-    const changedBook: Book = {
-      id: this.bookId, author, title
-    };
-    this.books.saveOrUpdate(changedBook)
-      .subscribe(() => this.router.navigate(['/books']));
+  save() {
+    if (this.bookForm.valid) {
+      const changedBook: Book = {
+        ...this.bookForm.value, id: this.bookId
+      };
+      this.books.saveOrUpdate(changedBook)
+        .subscribe(() => this.router.navigate(['/books']));
+    }
+  }
+
+  getErrorsOf(controlName: string) {
+    const errors = this.bookForm.get(controlName) && this.bookForm.get(controlName).errors;
+    if (errors) {
+      return Object.keys(errors)
+        .map(errorKey => {
+          switch (errorKey) {
+            case 'required': return 'Please provide a value';
+            case 'maxlength': {
+              const errorMeta = errors[errorKey];
+              return `Please provide value shorter than ${errorMeta.requiredLength} (currently ${errorMeta.actualLength} characters)`;
+            }
+            default: return 'Unknown error';
+          }
+        });
+    }
   }
 }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule, RouterModule
   ],
-  exports: [HeaderComponent, CommonModule, RouterModule]
+  exports: [HeaderComponent, CommonModule, RouterModule,
+    ReactiveFormsModule]
 })
 export class SharedModule {
 }
